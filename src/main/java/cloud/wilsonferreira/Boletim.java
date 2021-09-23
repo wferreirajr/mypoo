ce99ed85284acf1b99ba939374cf6f938ae6c014
Wilson Ferreira Junior <wilson.ferreira@bv.com.br>
2021-09-23 06:03:35 -0300

    Metodo costrutores

--- src/main/java/cloud/wilsonferreira/Boletim.java
+++ src/main/java/cloud/wilsonferreira/Boletim.java
@@ -25,6 +25,12 @@ public class Boletim {
         
     }
 
+    /**
+        The Desciption of the method to explain what the method does
+        @param the parameters used by the method
+        @return the value returned by the method
+        @throws what kind of exception does this method throw
+    */
     public double calcular_media(double AM, double AC, double AS) {
         double media = AM * 0.3 + AC * 0.2 + AS * 0.5;
         return media;
@@ -33,6 +39,24 @@ public class Boletim {
     public double calcular_media(double PM, double AS) {
         double media = PM * 0.4 + AS * 0.6;
         return media;
+    }
+
+    public Boletim(float n, int f, String s) {
+        setBoletim(n, f, s);
+    }
+
+    public Boletim(String st) {
+        this(10, 100, st);
+    }
+
+    public Boletim() {
+        this(0, 0, null);
+    }
+
+    public void setBoletim(float n, int f, String s) {
+        setNota(n);
+        setFrequencia(f);
+        setStatus(s);
     }
 
     public float getNota() {
@@ -57,6 +81,10 @@ public class Boletim {
 
     public void setStatus(String status) {
         this.status = status;
-    }    
+    }
+
+    public void imprimir() {
+        System.out.println(getNota() + "\n" + getFrequencia() + "\n" + getStatus() + "\n");
+    }
     
 }
--- src/main/java/cloud/wilsonferreira/Principal.java
+++ src/main/java/cloud/wilsonferreira/Principal.java
@@ -2,9 +2,35 @@ package cloud.wilsonferreira;
 
 import java.lang.ProcessHandle.Info;
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
+		float nota;
+		int freq;
+		String st;
+		
+		Scanner ler = new Scanner (System.in);
+		
+		System.out.println("Insira a nota de início");
+		nota = ler.nextFloat();
+		System.out.println("Insira a frequência de início");
+		freq = ler.nextInt();
+		System.out.println("Insira o status de início");
+		st = ler.next();
+		
+		Boletim B1 = new Boletim(nota,freq,st);
+		Boletim B2 = new Boletim();
+		Boletim B3 = new Boletim("APROVADO");
+		
+		System.out.println("Uso do construtor com 3 parâmetros: ");
+		B1.imprimir();
+		System.out.println("Uso do construtor com nenhum parâmetro: ");
+		B2.imprimir();
+		System.out.println("Uso do construtor com 2 parâmetros: ");
+		B3.imprimir();
+
+
         Pessoa P = new Pessoa();
 
         P.setNome("Wilson");
